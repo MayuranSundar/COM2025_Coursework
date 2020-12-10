936999cf8165e1b168f44882d40ef0aa5126d775
Mayuran Sundar <mayuransundar@hotmail.com>
2020-12-10 03:38:58 +0000

    Navigation bar updated

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,7 +4,7 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
-    assert_select 'nav.side_nav a', minimum: 5
+    assert_select 'nav.top_nav a', minimum: 5
     assert_select 'main ul.catalog li', 3
     assert_select 'h2', 'Apple iPhone 12 Pro'
     assert_select '.price', /\£[,\d]+\.\d\d/
