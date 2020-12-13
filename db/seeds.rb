d069ae84c309b9e0ceb4f1293fac15d0b456d62d
Mayuran Sundar <mayuransundar@hotmail.com>
2020-12-13 20:06:25 +0000

    Added a logo to navbar and as a favicon, finished updating db. ERROR WITH DB:SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ Phone.create!(
     size: '128GB',
     colour: 'Midnight Blue',
     specifications: 'iOS 14',
-    image_url: 'iphone12pro.jpg',
+    image_url: 'phones/iphone12pro.jpg',
     price: 999.00)     
 
 Phone.create!(
@@ -15,9 +15,91 @@ Phone.create!(
     size: '256GB',
     colour: 'Gold',
     specifications: 'iOS 14',
-    image_url: 'iphone12promax.jpg',
-    price: 1249.00) 
+    image_url: 'phones/iphone12promax.jpg',
+    price: 1249.00)
+    
+Phone.create!(
+    brand: 'Apple',
+    model: 'iPhone 12',
+    size: '64GB',
+    colour: 'Red',
+    specifications: 'iOS 14',
+    image_url: 'phones/iphone12.jpg',
+    price: 799.00) 
 
+Phone.create!(
+    brand: 'Samsung',
+    model: 'Galaxy S20 Ultra 5G',
+    size: '128GB',
+    colour: 'Black',
+    specifications: 'Android 10',
+    image_url: 'phones/s20ultra.jpg',
+    price: 1249.00) 
+    
+Phone.create!(
+    brand: 'Samsung',
+    model: 'Galaxy S20 FE',
+    size: '128GB',
+    colour: 'Cloud Navy',
+    specifications: 'Android 10',
+    image_url: 'phones/s20fe.jpg',
+    price: 599.99)     
+
+Phone.create!(
+    brand: 'Samsung',
+    model: 'Galaxy S20',
+    size: '128GB',
+    colour: 'Blue',
+    specifications: 'Android 10',
+    image_url: 'phones/s20.jpg',
+    price: 799.99) 
+    
+Phone.create!(
+    brand: 'Samsung',
+    model: 'Galaxy Fold2 5G',
+    size: '256GB',
+    colour: 'Mystic Bronze',
+    specifications: 'Android 10',
+    image_url: 'phones/fold2.jpg',
+    price: 1799.99) 
+
+Phone.create!(
+    brand: 'Samsung',
+    model: 'Galaxy Z Flip',
+    size: '256GB',
+    colour: 'Mystic Grey',
+    specifications: 'Android 10',
+    image_url: 'phones/zflip.jpg',
+    price: 1399.99) 
+
+Phone.create!(
+    brand: 'Google',
+    model: 'Pixel 4 a',
+    size: '128GB',
+    colour: 'Just Black',
+    specifications: 'Android 10',
+    image_url: 'phones/pixel4a.jpg',
+    price: 349.00) 
+
+Phone.create!(
+    brand: 'Google',
+    model: 'Pixel 5',
+    size: '128GB',
+    colour: 'Just Black',
+    specifications: 'Android 10',
+    image_url: 'phones/pixel5.jpg',
+    price: 599.00) 
+
+Phone.create!(
+    brand: 'Sony',
+    model: 'Xperia 1II',
+    size: '256GB',
+    colour: 'Black',
+    specifications: 'Android 10',
+    image_url: 'phones/xperia.jpg',
+    price: 1099.99) 
+
+User.delete_all
 User.create(
     name: 'admin',
     password: 'mypass'
